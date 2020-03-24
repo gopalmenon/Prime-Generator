4eb6084cab6d351121b81f383eabb371b15711e4
gopalmenon <gopal.mnn@gmail.com>
2020-03-24 17:01:15 -0600

    added tests and finished up

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-const FIRST_PRIME: u64 = 2;
+const FIRST_PRIME_LESS_ONE: u64 = 1;
 
 struct Prime {
     next_prime: u64,
@@ -7,8 +7,17 @@ struct Prime {
 impl Prime {
     fn new() -> Self {
         Prime {
-            next_prime: FIRST_PRIME,
+            next_prime: FIRST_PRIME_LESS_ONE,
         }
+    }
+    fn is_prime(&self, prime_candidate: u64) -> bool {
+        let upper_limit = (prime_candidate as f64).sqrt() as u64;
+        for divisor in 2..=upper_limit {
+            if prime_candidate % divisor == 0 {
+                return false;
+            }
+        }
+        true
     }
 }
 
@@ -16,18 +25,46 @@ impl Iterator for Prime {
     type Item = u64;
 
     fn next(&mut self) -> Option<Self::Item> {
-        // loop {
-        //     self.next_prime += 1;
-        //     break;
-        // }
+        loop {
+            self.next_prime += 1;
+            if self.is_prime(self.next_prime) {
+                break;
+            }
+        }
         Some(self.next_prime)
     }
 }
 
 #[cfg(test)]
 mod tests {
+
     #[test]
     fn first_prime() {
-        assert_eq!(super::FIRST_PRIME, super::Prime::new().next().unwrap());
+        assert_eq!(super::Prime::new().next().unwrap(), 2);
+    }
+
+    #[test]
+    fn second_prime() {
+        let mut prime_generator = super::Prime::new();
+        prime_generator.next();
+        assert_eq!(prime_generator.next().unwrap(), 3);
+    }
+
+    #[test]
+    fn tenth_prime() {
+        let mut prime_generator = super::Prime::new();
+        for _ in 1..10 {
+            prime_generator.next();
+        }
+        assert_eq!(prime_generator.next().unwrap(), 29);
+    }
+
+    #[test]
+    fn twentieth_prime() {
+        let mut prime_generator = super::Prime::new();
+        for _ in 1..20 {
+            prime_generator.next();
+        }
+        assert_eq!(prime_generator.next().unwrap(), 71);
     }
 }
